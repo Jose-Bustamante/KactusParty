42f0cefed10f83b17cd567305b5605959de548d0
Jose-Bustamante <jose.bustamante@eriks.nl>
2018-12-16 18:43:42 +0100

    Kactus02, gitIgnore node_modules, graphQl

---
+++ kactus01/build/precache-manifest.62fdc18512bbeb578e22eb106542ffb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a0cc11df20217733dff8",
+    "url": "/static/js/main.a0cc11df.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "a0cc11df20217733dff8",
+    "url": "/static/css/main.dc4aa264.chunk.css"
+  },
+  {
+    "revision": "a379ec95942a21dc2a6e1e4e8ff35b3b",
+    "url": "/index.html"
+  }
+];
